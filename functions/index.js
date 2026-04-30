a9d0a84a6a2b07d859e7dc58500c576012bf76c7
Paulo Salem <paulosalem8@gmail.com>
2026-04-30 01:36:46 -0500

    beta 1.51

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const { onCall, HttpsError } = require("firebase-functions/v2/https");
-const { defineSecret } = require("firebase-functions/params");
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-// Nota: La funcionalidad de envío de correos vía Resend ha sido removida.
-// Las invitaciones ahora se manejan exclusivamente mediante links directos.
-
